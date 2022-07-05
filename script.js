02d6975ef2941664f5dc4dc312310e17617d3bc1
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-07-05 21:05:29 +0200

    Add JS to Bookmark and Show-Answer-Button

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let button = document.querySelector(
-  '[data-js="quiz--show--answer--btn__clicked"]'
-);
+// SHOW ANSWER
 
-function buttonClicked() {
-  console.log("Button clicked");
+let button = document.querySelector('[data-js="answer-button"]');
+let answer = document.querySelector('[data-js="show-text"]');
+
+function buttonClick() {
+  answer.classList.toggle("answer-text");
 }
 
-button.addEventListener("click", buttonClicked);
+button.addEventListener("click", buttonClick);
+
+// BOOKMARK
+
+let bookmark = document.querySelector('[data-js="bookmark-checked"]');
+
+function bookmarkClick() {
+  bookmark.classList.toggle("quiz-card-bookmark--checked");
+}
+
+bookmark.addEventListener("click", bookmarkClick);
