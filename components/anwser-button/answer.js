fa68ec14400d67adb14726f7f16ab010ddc21545
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-07-07 18:38:03 +0200

    solve hide-show problem

--- components/anwser-button/answer.js
+++ components/anwser-button/answer.js
@@ -2,8 +2,12 @@ export default function Answer() {
   const button = document.querySelector('[data-js="answer-button"]');
   const answer = document.querySelector('[data-js="show-text"]');
 
+  let answerText = false;
+
   function buttonClick() {
     answer.classList.toggle("answer-text");
+    answerText = !answerText;
+    button.innerText = answerText ? "Hide Answer" : "Show Answer";
   }
 
   button.addEventListener("click", buttonClick);
